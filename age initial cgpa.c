d49be6f80d52dbccf00c0749f9c004db672d6be9
koreleparavaishnavi <92355542+koreleparavaishnavi@users.noreply.github.com>
2021-10-20 12:28:10 +0530

    Add files via upload

---
+++ age initial cgpa.c
@@ -0,0 +1,20 @@
+/*cprogram to print hello world*/
+#include<stdio.h>
+void main()
+{
+	//variable declaration
+	int age;
+	float cgpa;
+	char initial;
+age=21;
+cgpa=9.2;
+initial='k';
+
+printf("age of the student%d\n",age);//age of the student
+printf("cgpaof the student%f\n",cgpa);//cgpa of the student
+printf("initial of the student%c\n",initial);//initial of the student
+}
+	
+	
+
+
